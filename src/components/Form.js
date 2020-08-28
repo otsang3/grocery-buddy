08ded55ff72e2c4f5f9137fca0572fdaa500aae6
otsang3 <ohhtsang@gmail.com>
2020-08-28 18:54:47 +0100

    add files

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Form() {
+
+    return(
+        <div>
+            <input/>
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default Form;
